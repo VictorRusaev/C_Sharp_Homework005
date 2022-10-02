a40e979bc2a92730fa5194b2d5c7dd6f4bda3140
Victor Rusaev <rusaev74@mail.ru>
2022-10-02 15:49:02 +0300

    TASK001 even from three-digit numbers

---
+++ TASK001_even_from_three_digit_numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Clear();
+
+int[] mainArray = CreateArray();
+Console.Write(String.Join(", ", mainArray));
+int numOfEv = EvenFromArray(mainArray);
+Console.WriteLine();
+Console.WriteLine(numOfEv);
+
+int[] CreateArray()
+{
+    int[] array = new int[10];
+    Random randGenerator = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = randGenerator.Next(100, 1000);
+    }
+    return array;
+}
+
+int EvenFromArray(int[] arr)
+{
+    int numberOfEven = 0;
+    for(int i = 0; i < arr.Length; i++)
+    {
+        if(arr[i] % 2 == 0)
+        {
+            numberOfEven = numberOfEven + 1;
+        }
+    }
+    return numberOfEven;
+}
+
